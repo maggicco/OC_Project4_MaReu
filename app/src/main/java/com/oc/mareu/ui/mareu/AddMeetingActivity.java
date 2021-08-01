ea8722f9ac2a5275c180d625d8a906ca92f93e99
maggicco <maggicco83@hotmail.fr>
2021-08-01 12:24:23 +0200

    Added AddMeetingActivity class

---
+++ app/src/main/java/com/oc/mareu/ui/mareu/AddMeetingActivity.java
@@ -0,0 +1,16 @@
+package com.oc.mareu.ui.mareu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.oc.mareu.R;
+
+public class AddMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meeting);
+    }
+}
