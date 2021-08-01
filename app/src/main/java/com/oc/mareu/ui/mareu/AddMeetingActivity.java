22b1fcb1ca703dc15e69c2e730dbebd82bb55ead
maggicco <maggicco83@hotmail.fr>
2021-08-01 18:45:42 +0200

    setting addMeeting class and layout

--- app/src/main/java/com/oc/mareu/ui/mareu/AddMeetingActivity.java
+++ app/src/main/java/com/oc/mareu/ui/mareu/AddMeetingActivity.java
@@ -3,6 +3,8 @@ package com.oc.mareu.ui.mareu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.oc.mareu.R;
 
@@ -16,5 +18,24 @@ public class AddMeetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_meeting);
         //Back Home arrow
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_room);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterRoom = ArrayAdapter.createFromResource(this,
+                R.array.spinner_room, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterRoom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapterRoom);
+
+        Spinner spinnerColor = (Spinner) findViewById(R.id.spinner_color);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterColor = ArrayAdapter.createFromResource(this,
+                R.array.spinner_color, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterColor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerColor.setAdapter(adapterColor);
     }
 }
