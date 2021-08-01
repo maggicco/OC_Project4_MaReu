eedc9d296105a5544f0c422c2de320c083cc6a3c
maggicco <maggicco83@hotmail.fr>
2021-08-01 13:48:24 +0200

    Added intent to new AddMeeting and general changes

--- app/src/main/java/com/oc/mareu/ui/mareu/ListMeetingActivity.java
+++ app/src/main/java/com/oc/mareu/ui/mareu/ListMeetingActivity.java
@@ -2,8 +2,11 @@ package com.oc.mareu.ui.mareu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.oc.mareu.R;
 
 public class ListMeetingActivity extends AppCompatActivity {
@@ -12,5 +15,18 @@ public class ListMeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_meeting);
+
+        FloatingActionButton fab = findViewById(R.id.add_meeting);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListMeetingActivity.this, AddMeetingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
