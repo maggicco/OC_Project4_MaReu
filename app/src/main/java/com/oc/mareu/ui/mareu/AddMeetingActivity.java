3f746f455ee2b37ed8244ad1aa5f89a78d3cbdc5
maggicco <maggicco83@hotmail.fr>
2021-08-01 14:40:38 +0200

    Set basic fragment layouts

--- app/src/main/java/com/oc/mareu/ui/mareu/AddMeetingActivity.java
+++ app/src/main/java/com/oc/mareu/ui/mareu/AddMeetingActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 
 import com.oc.mareu.R;
 
+import java.util.Objects;
+
 public class AddMeetingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meeting);
+        //Back Home arrow
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
