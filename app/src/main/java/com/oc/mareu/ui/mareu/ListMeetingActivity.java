930e0df583d607ca83b255e6a1b20994f227029b
maggicco <maggicco83@hotmail.fr>
2021-08-03 18:16:22 +0200

    trying to restart activity on orientation chenged to fix

--- app/src/main/java/com/oc/mareu/ui/mareu/ListMeetingActivity.java
+++ app/src/main/java/com/oc/mareu/ui/mareu/ListMeetingActivity.java
@@ -1,11 +1,14 @@
 package com.oc.mareu.ui.mareu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.oc.mareu.R;
@@ -34,5 +37,21 @@ public class ListMeetingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    // TODO: 03/08/2021 restart activity after orientation changed 
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        onRestart();
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+                    onDestroy();
+        }
+//        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+//            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+//        }
+        //setContentView(R.layout.activity_list_meeting);
     }
 }
